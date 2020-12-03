489c24b1007d08a1c706836e55018378119b9e73
barto <bartolomezambranaperez99@gmail.com>
2020-12-03 16:37:25 +0100

    facetas incluidas en indexación

--- Indexador/src/indexador/Index.java
+++ Indexador/src/indexador/Index.java
@@ -14,19 +14,26 @@ import org.apache.lucene.document.IntPoint;
 import org.apache.lucene.document.StoredField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.facet.FacetField;
+import org.apache.lucene.facet.FacetsConfig;
+import org.apache.lucene.facet.taxonomy.directory.DirectoryTaxonomyWriter;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.FSDirectory;
+
 
 /**
  * @author Bartolomé Zambrana Pérez y Alonso Bueno Herrero.
  */
 public class Index {
     // Variables
-    private final String indexPath = "./../P3/indices/"; //Escribimos en la carpeta de índices de la aplicación buscador
+    private final String indexPath = "./../P3/indices/"; //Escribimos en la carpeta de índices de la aplicación buscador.
+    private final String facetPath = "./../P3/facetas/"; //Escribimos en la carpeta de facetas de la aplicación buscador.
     private HashMap<String, Analyzer> analyzerPerField = new HashMap<String, Analyzer>();
     private PerFieldAnalyzerWrapper analyzer = null;
     private IndexWriter writer;
+    private FacetsConfig fconfig;
+    private DirectoryTaxonomyWriter taxoWriter;
     
     // Constructor del índice
     public Index() throws IOException{
@@ -38,20 +45,31 @@ public class Index {
         analyzerPerField.put("text", new EnglishAnalyzer());
         
         analyzer = new PerFieldAnalyzerWrapper(new WhitespaceAnalyzer(), analyzerPerField);
-        configIndex();
+        configurarIndice();
+        configurarFacetas();
     }
     
-    private void configIndex() throws IOException{
+    private void configurarIndice() throws IOException{
         FSDirectory dir = FSDirectory.open(Paths.get(indexPath));
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
         writer = new IndexWriter(dir,config);
     }
     
+    private void configurarFacetas() throws IOException{
+        FSDirectory taxoDir = FSDirectory.open(Paths.get(facetPath));
+        fconfig = new FacetsConfig();
+        taxoWriter = new DirectoryTaxonomyWriter(taxoDir);
+        
+        fconfig.setMultiValued("institution", true);
+        
+    }
+    
     private void closeIndex() throws IOException{
         try{
             writer.commit();
             writer.close();
+            taxoWriter.close();
         }catch(IOException e){
             System.out.println("ERROR IN CLOSE INDEX: " + e);
         }
@@ -61,12 +79,17 @@ public class Index {
         for(Json json : documentos){    
             Document doc = new Document();
             
+
+            //INCLUIMOS LOS ELEMENTOS AL ÍNDICE
+            
             //Nombre documento
             doc.add(new StringField("namefile", json.getNameFile(), Field.Store.YES));
             //Autor e institución
             for(int j = 0 ; j <  json.getAuthors().size(); j++){
                doc.add(new TextField("author",json.getAuthors().get(j).getKey(), Field.Store.YES));               
                doc.add(new TextField("institution",json.getAuthors().get(j).getValue(),Field.Store.YES));
+               //Faceta institución
+               doc.add(new FacetField("institution",json.getAuthors().get(j).getValue()));
                
             }
             
@@ -79,13 +102,16 @@ public class Index {
             //Tamaño del fichero
             doc.add(new IntPoint("size",json.getSizeFile()));
             doc.add(new StoredField("size",json.getSizeFile()));
+            //Faceta tamaño
+            doc.add(new FacetField("institution",json.getSizeFile().toString()));
             //Descripción
             doc.add(new TextField("brief", json.getBrief(),Field.Store.YES));
             //Contenido
             doc.add(new TextField("text", json.getText(), Field.Store.NO));
+
             
-            writer.addDocument(doc);
-            
+            //writer.addDocument(doc);
+            writer.addDocument(fconfig.build(taxoWriter,doc));
         }
         
         closeIndex();
